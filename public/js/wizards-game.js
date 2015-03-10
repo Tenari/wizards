d9001ba3da9fd4df0f59db5d1de0211437df9f94
Daniel Zapata <daniel@peoplepattern.com>
2015-03-09 19:12:10 -0500

    inital commit

---
+++ public/js/wizards-game.js
@@ -0,0 +1,29 @@
+// setup scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// build cube
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x5C4B27 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+// run game
+function render() {
+  // rotate cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
+  cube.position.z -= 0.01;
+  requestAnimationFrame( render );
+  renderer.render( scene, camera );
+}
+render();
