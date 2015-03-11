dac1303c4b32439b9e99a33ccecbc09987bee3a8
Daniel Zapata <daniel@peoplepattern.com>
2015-03-11 08:45:20 -0500

    server tells client how to build scene

--- public/js/wizards-game.js
+++ public/js/wizards-game.js
@@ -3,33 +3,42 @@ var socket = io.connect('http://localhost:3000');
 socket.on('startGame', function( data ) { // the 'OK, game has been joined, now you need to init it' message from the server
   //Note that the data is the object we sent from the server, as is. So we can assume its id exists. 
   console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.uuid );
-  startGame(data);
+  
+  startGame(data.path);
 });
-/* socket.on('move', function(data){
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
-}); */
 
-function startGame(setupData) {
+var request, renderer, scene, camera;
+function startGame(path) {
+  var getter = new THREE.XHRLoader();
+  getter.load(path , function ( text ) {
+    initGame( JSON.parse( text ) );
+    play();
+  });
+}
+
+function initGame(data){
   // setup scene
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var loader = new THREE.ObjectLoader();
+  scene = loader.parse(data.scene); //new THREE.Scene();
+  camera = loader.parse(data.camera);//new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  var renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer( { antialias: true } ); // has to be available to animate
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  // build cube
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x5C4B27 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
   // run game
-  function render() {
+  /*function render() {
     requestAnimationFrame( render );
     renderer.render( scene, camera );
-  }
+  }*/
+
 }
+
+function play() {
+  request = requestAnimationFrame( animate );
+}
+var animate = function ( time ) {
+  request = requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+};
--- socket.js
+++ socket.js
@@ -1,8 +1,9 @@
 var UUID = require('node-uuid');
 module.exports = function(sio){
   sio.sockets.on('connection', function (client) {
+    console.log(client);
     client.userid = UUID();
-    client.emit('startGame', { uuid: client.userid } );
+    client.emit('startGame', { uuid: client.userid, path: '/data/app.json' } );
 /*    var interval = setInterval(function(){
       client.emit('move', {eval: 'cube.position.x += 0.02'})
     }, 100); */
