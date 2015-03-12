8e307f2b51cf1a0fc72da3ba98cf5234a6cf2d5d
Daniel Zapata <daniel@peoplepattern.com>
2015-03-11 21:16:55 -0500

    client dumps events to server over socket.io

--- public/js/wizards-game.js
+++ public/js/wizards-game.js
@@ -1,13 +1,25 @@
+// global variable declarations
+var request
+  , renderer
+  , scene
+  , camera
+  , eventRecord = []
+  , socket = io.connect('http://localhost:3000')
+  , gameroom = ''
+  , uid
+  ;
 // socket.io stuff
-var socket = io.connect('http://localhost:3000');
 socket.on('startGame', function( data ) { // the 'OK, game has been joined, now you need to init it' message from the server
   //Note that the data is the object we sent from the server, as is. So we can assume its id exists. 
   console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.uuid );
-  
+  uid = data.uuid;
+  // join the game room
+//  socket.join(''+data.game);
+//  gameroom = ''+data.game;
+  // do the three js initialization based on what the server told us to do
   startGame(data.path);
 });
 
-var request, renderer, scene, camera;
 function startGame(path) {
   var getter = new THREE.XHRLoader();
   getter.load(path , function ( text ) {
@@ -27,12 +39,29 @@ function initGame(data){
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  // run game
-  /*function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
-  }*/
-
+  // add event listeners
+  var eventList = ['keyup', 'keydown', 'mousedown', 'mouseup', 'mousemove'];
+  for (var i = 0; i < eventList.length; i++) {
+    document.addEventListener(eventList[i], function(event){
+      eventRecord.push({
+        owner: uid,
+        type: event.type,
+        timeStamp: event.timeStamp,
+        keyCode: event.keyCode,
+        button: event.button,
+        altKey: event.altKey,
+        charCode: event.charCode,
+        ctrlKey: event.ctrlKey,
+        metaKey: event.metaKey,
+        movementX: event.movementX,
+        movementY: event.movementY,
+        shiftKey: event.shiftKey,
+        which: event.which,
+        x: event.x,
+        y: event.y
+      })
+    });
+  }
 }
 
 function play() {
@@ -40,5 +69,12 @@ function play() {
 }
 var animate = function ( time ) {
   request = requestAnimationFrame( animate );
+  // send events to the server
+  if (eventRecord.length > 0) {
+    socket.emit('events', {list: eventRecord});
+    eventRecord = []; // kill the events we just emitted
+  }
+  // render the image
   renderer.render( scene, camera );
 };
+
--- socket.js
+++ socket.js
@@ -1,16 +1,19 @@
 var UUID = require('node-uuid');
 module.exports = function(sio){
   sio.sockets.on('connection', function (client) {
-    console.log(client);
+    var game = UUID();
     client.userid = UUID();
-    client.emit('startGame', { uuid: client.userid, path: '/data/app.json' } );
-/*    var interval = setInterval(function(){
-      client.emit('move', {eval: 'cube.position.x += 0.02'})
-    }, 100); */
+    client.emit('startGame', { uuid: client.userid, path: '/data/app.json', game: game } );
     console.log('- socket.io:: player ' + client.userid + ' connected');
     client.on('disconnect', function () {
       console.log('- socket.io:: client disconnected ' + client.userid );
-//      clearInterval(interval);
     });
+    client.on('events', function(data){
+      /*var typelist = [];
+      for (var i =0; i< data.list.length; i++) {
+        typelist.push(data.list[i].type)
+      }
+      console.log(typelist);*/
+    })
   });
 }
